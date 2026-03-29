2ba9307dab787aa02efddd068d9270ec92d91734
Matthew Hurley <matthew.hurley953@gmail.com>
2026-03-28 20:12:54 -0400

    lint-staged added & eslint config modified (#4)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = defineConfig([
   {
     rules: {
       'no-console': ['error', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   },
   eslintConfigPrettier,
