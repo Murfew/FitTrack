119ee894f1b1896c43789f07b85035487cc0e478
Murfew <matthew.hurley953@gmail.com>
2026-04-14 17:10:07 -0400

    create seed script and update prisma config

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { prisma } from '@/lib/prisma';
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'user@example.com' },
+    update: {},
+    create: {
+      email: 'user@example.com',
+      name: 'Example User',
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
