6d5a1f7c5f88c6359b4b67e86b1aab00bc71fc07
Murfew <matthew.hurley953@gmail.com>
2026-04-16 11:59:50 -0400

    update var validation, and exclude .env.example from gitignore

--- src/env.ts
+++ src/env.ts
@@ -8,11 +8,11 @@ export const env = createEnv({
     AUTH_GITHUB_SECRET: z
       .string()
       .length(40)
-      .regex(/^[a-f0-9]+$/),
+      .regex(/^[a-fA-F0-9]+$/),
     AUTH_GOOGLE_ID: z
       .string()
       .regex(/^\d+-[a-z0-9]+\.apps\.googleusercontent\.com$/),
-    AUTH_GOOGLE_SECRET: z.string().regex(/^GOCSPX-[A-Za-z0-9_-]{28}$/),
+    AUTH_GOOGLE_SECRET: z.string().regex(/^GOCSPX-[A-Za-z0-9_-]{24,}$/),
     AUTH_SECRET: z.string().min(32),
   },
 
